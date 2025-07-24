e46fa1e7e8bf50e73647e6e3fb06cd46ed2a11b9
phuvu <phu270904@gmail.com>
2025-07-24 21:28:43 +0700

    first commit

---
+++ config/firebase.config.js
@@ -0,0 +1,21 @@
+// firebaseConfig.js
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDISV-faBMEpGdiGkoGm1U-OFiN7Wf5LsU",
+  authDomain: "universalyoga-66b5b.firebaseapp.com",
+  projectId: "universalyoga-66b5b",
+  storageBucket: "universalyoga-66b5b.firebasestorage.app",
+  messagingSenderId: "894002154689",
+  appId: "1:894002154689:web:da7fe3addc69d6acf35840",
+  measurementId: "G-QETM3FDCGH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = getFirestore(app);
+
+export { db };
